23e59d73579ace2335d88d39fe5b6fcc066893d5
Usuário Convidado <Guest@L2602MICRO12.local>
2018-08-25 14:42:59 -0300

    Projeto 8

---
+++ Exemplo2 TableViewController/Aviso.swift
@@ -0,0 +1,28 @@
+//
+//  Aviso.swift
+//  Exemplo2 TableViewController
+//
+//  Created by Usuário Convidado on 25/08/2018.
+//  Copyright © 2018 Julio Augusto. All rights reserved.
+//
+
+import UIKit
+
+class Aviso: NSObject {
+    
+    static func exibirMensagem(msg:String, sender:AnyObject){
+        let alerta = UIAlertController(
+            title: "Aviso!",
+            message: msg,
+            preferredStyle: UIAlertControllerStyle.alert)
+        
+        alerta.addAction(UIAlertAction(
+            title: "OK",
+            style: UIAlertActionStyle.default,
+            handler: nil))
+        
+        sender.present(alerta, animated:true, completion:nil)
+        
+    }
+
+}
